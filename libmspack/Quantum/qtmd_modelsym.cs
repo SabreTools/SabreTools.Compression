5477afaf1e54acd178f703d8b541b3a017dd9f0e
Matt Nadareski <mnadareski@outlook.com>
2023-09-21 21:31:45 -0400

    Remove partially ported libmspack code

--- libmspack/Quantum/qtmd_modelsym.cs
+++
@@ -1,9 +0,0 @@
-namespace SabreTools.Compression.libmspack
-{
-    public class qtmd_modelsym
-    {
-        public ushort sym { get; set; }
-
-        public ushort cumfreq { get; set; }
-    }
-}
--- libmspack/mspack_file.cs
+++
@@ -1,9 +0,0 @@
-namespace SabreTools.Compression.libmspack
-{
-    /// <summary>
-    /// A structure which represents an open file handle. The contents of this
-    /// structure are determined by the implementation of the
-    /// mspack_system::open() method.
-    /// </summary>
-    public unsafe abstract class mspack_file { }
-}
