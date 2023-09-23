3010a0523c7c8c91153446cb1ad467e86ccd831c
Matt Nadareski <mnadareski@outlook.com>
2023-09-23 11:16:11 -0400

    Use local constants until Models updated

--- Quantum/Decompressor.cs
+++ Quantum/Decompressor.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using SabreTools.Models.Compression.Quantum;
-using static SabreTools.Models.Compression.Quantum.Constants;
+using static SabreTools.Compression.Quantum.Constants;
 
 namespace SabreTools.Compression.Quantum
 {
@@ -221,25 +221,25 @@ namespace SabreTools.Compression.Quantum
                     {
                         case 4:
                             int model4sym = GetSymbol(_model4);
-                            int model4extra = _bitStream.ReadBitsMSB(PositionExtraBits[model4sym]);
+                            int model4extra = (int)(_bitStream.ReadBitsMSB(PositionExtraBits[model4sym]) ?? 0);
                             offset = PositionSlot[model4sym] + model4extra + 1;
                             length = 3;
                             break;
                         
                         case 5:
                             int model5sym = GetSymbol(_model5);
-                            int model5extra = _bitStream.ReadBitsMSB(PositionExtraBits[model5sym]);
+                            int model5extra = (int)(_bitStream.ReadBitsMSB(PositionExtraBits[model5sym]) ?? 0);
                             offset = PositionSlot[model5sym] + model5extra + 1;
                             length = 4;
                             break;
 
                         case 6:
                             int lengthSym = GetSymbol(_model6len);
-                            int lengthExtra = _bitStream.ReadBitsMSB(LengthExtraBits[lengthSym]);
+                            int lengthExtra = (int)(_bitStream.ReadBitsMSB(LengthExtraBits[lengthSym]) ?? 0);
                             length = LengthSlot[lengthSym] + lengthExtra + 5;
 
                             int model6sym = GetSymbol(_model6);
-                            int model6extra = _bitStream.ReadBitsMSB(PositionExtraBits[model6sym]);
+                            int model6extra = (int)(_bitStream.ReadBitsMSB(PositionExtraBits[model6sym]) ?? 0);
                             offset = PositionSlot[model6sym] + model6extra + 1;
                             break;
 
