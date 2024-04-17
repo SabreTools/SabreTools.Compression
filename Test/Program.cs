f79b5353f77d96ebc0289df0e2297095344ee1db
Matt Nadareski <mnadareski@outlook.com>
2024-04-16 22:06:54 -0400

    Add empty test project

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using SabreTools.Compression;
+
+namespace Test
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // No implementation, used for experimentation
+        }
+    }
+}
